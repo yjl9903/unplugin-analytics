54e05212fde830399fc475720e1540f049c35f94
XLor <yjl9903@vip.qq.com>
2024-02-27 17:17:02 +0800

    feat(vite): disable when dev

--- packages/vite-plugin-analytics/src/index.ts
+++ packages/vite-plugin-analytics/src/index.ts
@@ -1,19 +1,32 @@
-import type { Plugin } from 'vite';
+import type { Plugin, ResolvedConfig } from 'vite';
 
 import { type AnalyticsOptions, generate } from '@unplugin-analytics/core';
 
 import { renderScriptTag } from './render';
 
 export interface Options {
+  /**
+   * Whether inject scripts during development
+   */
+  dev?: boolean;
+
   analytics?: AnalyticsOptions;
 }
 
 export default function Analytics(options: Options = {}): Plugin {
+  let config: ResolvedConfig;
   const tags = generate(options.analytics ?? []);
 
   return {
     name: 'vite-plugin-analytics',
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
     transformIndexHtml() {
+      // Skip transform when dev is true and during serve mode
+      if (options.dev && config.command === 'serve') {
+        return;
+      }
       return tags.map(renderScriptTag);
     }
   };
