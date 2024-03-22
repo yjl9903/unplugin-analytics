a2b8d01d9a18035641df4c2fd850d832f7df0297
XLor <yjl9903@vip.qq.com>
2024-03-22 15:20:27 +0800

    fix: testcases

--- packages/unplugin-analytics/test/build.test.ts
+++ packages/unplugin-analytics/test/build.test.ts
@@ -14,7 +14,7 @@ describe(
       const indexHtml = await fs.promises.readFile(path.join(root, 'dist', 'index.html'), 'utf-8');
 
       const expected = [
-        `<script defer data-website-id="a8602a4a-8d41-4df7-9797-5bd074785f2c" src="https://umami.onekuma.cn/script.js"></script>`,
+        `<script defer data-website-id="19800483-25c2-4fde-8330-3e717591eabb" src="https://umami.onekuma.cn/script.js"></script>`,
         `<script defer data-domain="garden.onekuma.cn" src="https://plausible.io/js/script.js"></script>`,
         `<script defer data-cf-beacon="{&quot;token&quot;: &quot;aa68fa3bf166467082bc79ba029b057f&quot;}" src="https://static.cloudflareinsights.com/beacon.min.js"></script>`,
         `(window, document, "clarity", "script", "kwj19d7z4j")`
@@ -30,7 +30,7 @@ describe(
       const indexHtml = await fs.promises.readFile(path.join(root, 'dist', 'index.html'), 'utf-8');
 
       const expected = [
-        `<script src="https://umami.onekuma.cn/script.js" defer data-website-id="a8602a4a-8d41-4df7-9797-5bd074785f2c"></script>`,
+        `<script src="https://umami.onekuma.cn/script.js" defer data-website-id="19800483-25c2-4fde-8330-3e717591eabb"></script>`,
         `<script src="https://plausible.io/js/script.js" defer data-domain="garden.onekuma.cn"></script>`,
         `<script src="https://static.cloudflareinsights.com/beacon.min.js" defer data-cf-beacon="{&quot;token&quot;: &quot;aa68fa3bf166467082bc79ba029b057f&quot;}"></script>`,
         `(window, document, "clarity", "script", "kwj19d7z4j")`
--- packages/vite-plugin-analytics/test/build.test.ts
+++ packages/vite-plugin-analytics/test/build.test.ts
@@ -15,7 +15,7 @@ describe('vite', () => {
     // @ts-ignore
     const indexHtml = output.output.filter((file) => file.fileName === 'index.html')[0];
     const expected = [
-      `<script defer data-website-id="a8602a4a-8d41-4df7-9797-5bd074785f2c" src="https://umami.onekuma.cn/script.js"></script>`,
+      `<script defer data-website-id="19800483-25c2-4fde-8330-3e717591eabb" src="https://umami.onekuma.cn/script.js"></script>`,
       `<script defer data-domain="garden.onekuma.cn" src="https://plausible.io/js/script.js"></script>`,
       `<script defer data-cf-beacon="{&quot;token&quot;: &quot;aa68fa3bf166467082bc79ba029b057f&quot;}" src="https://static.cloudflareinsights.com/beacon.min.js"></script>`,
       `(window, document, "clarity", "script", "kwj19d7z4j")`
